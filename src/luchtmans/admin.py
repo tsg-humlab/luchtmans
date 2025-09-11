43720e883e53c5dc2a3d0ad927a72375ed661128
Micha Hulsbosch <michahulsbosch@gmail.com>
2025-09-11 14:22:45 +0200

    feat: Add basic admin stuff

--- src/luchtmans/admin.py
+++ src/luchtmans/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Country, Place, Street, Address, Person, PersonPersonRelation, RelationType
+
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Street)
+class StreetAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(PersonPersonRelation)
+class PersonPersonRelationAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(RelationType)
+class RelationTypeAdmin(admin.ModelAdmin):
+    pass
