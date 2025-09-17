e2a2e3e72eacd201a3501f155353852d3e842abc
Micha Hulsbosch <michahulsbosch@gmail.com>
2025-09-17 12:18:40 +0200

    fix: Add CSRF_TRUSTED_ORIGINS

--- src/lm_project/settings.py
+++ src/lm_project/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', False)
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
-
+CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS')
 
 LANGUAGES = [
     ("en", _("English")),
