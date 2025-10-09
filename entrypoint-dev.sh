10c5c664af9b236d57905ec81758821c88453148
Micha Hulsbosch <michahulsbosch@gmail.com>
2025-10-09 09:27:40 +0200

    refactor: Remove entrypoint-dev.sh

--- entrypoint-dev.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Creating diretories here instead of in Dockerfile for backwards compatibility
-mkdir /app/writable/media
-mkdir /app/writable/log
-
-python manage.py runserver 0.0.0.0:${DJANGO_PORT}
