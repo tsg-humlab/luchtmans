78007e2ac25848873f114ac674299ffae2988996
Micha Hulsbosch <michahulsbosch@gmail.com>
2025-10-09 14:24:58 +0200

    feat: Extend admin functionality for Work

--- src/luchtmans/admin.py
+++ src/luchtmans/admin.py
@@ -60,8 +60,34 @@ class ReligionAdmin(TranslationAdmin):
     pass
 
 # Register empty admin classes in one go
-for model in [Language, GenreParisianCategory, Work, PersonWorkRelationRole, PersonWorkRelation, Format, STCNGenre,
+for model in [PersonWorkRelationRole, PersonWorkRelation, Format, STCNGenre,
               Edition, PersonEditionRelationRole, PersonEditionRelation, Collection, ItemType, Page, Binding, Item]:
     base_class = TranslationAdmin if model.__base__ == UniqueNameModel else admin.ModelAdmin
     admin_class = type(model.__name__+'Admin', (base_class,), {})
     admin.site.register(model, admin_class)
+
+
+@admin.register(Language)
+class LanguageAdmin(TranslationAdmin):
+    search_fields = ['name']
+
+
+@admin.register(GenreParisianCategory)
+class GenreParisianCategoryAdmin(TranslationAdmin):
+    search_fields = ['name']
+
+
+@admin.register(Work)
+class WorkAdmin(admin.ModelAdmin):
+    list_display = ['title', 'authors_list', 'uncertain', 'language_list', 'viaf_id', 'genre_parisian_category', 'notes']
+    search_fields = ['title']
+    list_filter = ['uncertain', 'languages', 'genre_parisian_category']
+    autocomplete_fields = ['languages', 'genre_parisian_category']
+
+    @admin.display(description=_("authors"))
+    def authors_list(self, obj):
+        return ", ".join(obj.authors.values_list('short_name', flat=True))
+
+    @admin.display(description=_("languages"))
+    def language_list(self, obj):
+        return ", ".join(obj.languages.values_list('name', flat=True))
