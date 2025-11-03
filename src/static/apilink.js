83e969bba7d742848860690401365612de8d67de
Micha Hulsbosch <michahulsbosch@gmail.com>
2025-11-03 12:39:14 +0100

    fix: Allow all .django-select2 elements to have HTML

--- src/static/apilink.js
+++ src/static/apilink.js
@@ -1,7 +1,7 @@
 jQuery('ready', () => {
     // Allow HTML in options
-    django.jQuery('#id_wikidata_id').select2.defaults.defaults.templateResult = (option) => django.jQuery(option.text);
-    django.jQuery('#id_wikidata_id').select2.defaults.defaults.templateSelection = (option) => django.jQuery(option.text);
+    django.jQuery('.django-select2').select2.defaults.defaults.templateResult = (option) => django.jQuery(option.text);
+    django.jQuery('.django-select2').select2.defaults.defaults.templateSelection = (option) => django.jQuery(option.text);
 
     // Change API link
     django.jQuery('.django-select2-apilink').on('change', (e) => {
