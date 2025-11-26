7ca73a5aa3523fd4498cbb8ae7218304eb5ff526
Micha Hulsbosch <michahulsbosch@gmail.com>
2025-11-26 11:01:39 +0100

    feat: redirect to admin

--- src/luchtmans/urls.py
+++ src/luchtmans/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from django.views.generic.base import RedirectView
 
 from .views import WikidataSuggestView, FillFieldsView
 
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/admin')),
     path('wikidata/', WikidataSuggestView.as_view(), name='wikidata'),
     path('fill_fields/<fill_field_name>/', FillFieldsView.as_view(), name='fill_fields'),
 ]
